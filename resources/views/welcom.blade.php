7e6baca2494605939f88d7901f80547243ff7712
Hager-Waleed <hager.waled.soliman@gmail.com>
2021-12-10 16:13:28 +0200

    Datebase_v2

--- resources/views/welcom.blade.php
+++ resources/views/welcom.blade.php
@@ -1,6 +1,35 @@
-@extends('MainPage')
+<!DOCTYPE html>
+<head>
+     <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@section('content')
+    <!-- Bootstrap CSS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/v4-shims.css">
+    <script src="https://kit.fontawesome.com/8d4c067090.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="main.css">
+    <title>The Outlet | Ecommerce Website</title>
+</head>
+    <style>
+        svg {
+                color: #fff;
+                fill: currentColor;
+                width: 24px;
+                height: 24px;
+                padding: 10px;
+        }
+        p{
+            color:#555;
+        }
+        
+    </style>
+<body>
 <div class="headerx">
         <div class = "containerx">
             <div class="navbarx">
@@ -257,5 +286,5 @@
             <hr><p class="copyrightx">Copyright 2021- Ecommerce Website ASU</p>
         </div>
     </div>
-@endsection
-
+</body>    
+</html>
